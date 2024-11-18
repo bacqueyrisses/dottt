a338cf2ba7ddcfd02572f067e3939b864e0b475d
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-11-18 11:23:11 +0800

    chore: remove unused logic

--- components/cards-wrapper.tsx
+++ components/cards-wrapper.tsx
@@ -1,10 +1,7 @@
 "use client";
-import { useSearchParams } from "next/navigation";
 import IconCard from "@/components/icon-card";
 
 export default function CardsWrapper() {
-  const searchParams = useSearchParams();
-  const search = searchParams.get("search");
   return (
     <section className={"flex flex-wrap gap-4 justify-center"}>
       <IconCard />
